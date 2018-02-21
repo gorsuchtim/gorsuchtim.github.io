61d33394f3d49e1bc302a1d2226c54f483864ca9
Tim Gorsuch <gorsuchtim@gmail.com>
2018-02-21 15:29:52 -0600

    update last tutorial card text 2

--- jstemplates/rps.js
+++ jstemplates/rps.js
@@ -266,7 +266,7 @@ $(document).ready(function () {
                 break;
             case 14:
                 tutorialCard = 14;
-                $('.tooltip__text').html('This is just a simple but interesting demo of how we can leverage growing data with trends to create more fluid or direct user experiences.');
+                $('.tooltip__text').html('This is a simple but interesting demo of how we can leverage growing data with trends to create more fluid or direct user experiences.');
                 break;
         }
     }

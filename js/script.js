1a8d3069aea1c4143c5b1c30d4932aa8e53fac41
Tim Gorsuch <gorsuchtim@gmail.com>
2018-11-20 21:33:17 -0600

    refactor

--- js/script.js
+++ js/script.js
@@ -1,75 +1,80 @@
 'use strict'
 
-function flashColor() {
-  var animate = {
-    getRandomNumber(arr) {
-      var randomNumber = Math.floor(arr.length * Math.random());
-      return randomNumber;
-    },
-    getRandomTile() {
-      var tile = tiles[Math.floor(Math.random() * tiles.length)];
-      return tile;
-    },
-    addColor(tile, color) {
-      tile.classList.add(color);
-    },
-    removeColor(tile, color) {
-      tile.classList.remove(color);
-    },
-  }
-  var randomNumber = animate.getRandomNumber(tiles);
-  var randomTile = animate.getRandomTile(randomNumber);
+function startFlash() {
+  var randomNumber = animateTiles.getRandomNumber(tiles);
+  var randomTile = animateTiles.getRandomTile(randomNumber);
   var colorsArray = ['red', 'green', 'blue', 'orange', 'yellow'];
-  var randomColor = animate.getRandomNumber(colorsArray);
-  animate.addColor(randomTile, colorsArray[randomColor]);
+  var randomColor = animateTiles.getRandomNumber(colorsArray);
+  animateTiles.addColorClass(randomTile, colorsArray[randomColor]);
 
   // use 825 for bach
   // use 425.531914893617 for figaro or 851.063829787234
 
   setTimeout(() => {
-    animate.removeColor(randomTile, colorsArray[randomColor]);
-    flashColor();
+    animateTiles.removeColorClass(randomTile, colorsArray[randomColor]);
+    startFlash();
   }, 925);
 }
 
+
+var animateTiles = {
+  getRandomNumber(arr) {
+    var randomNumber = Math.floor(arr.length * Math.random());
+    return randomNumber;
+  },
+  getRandomTile() {
+    var tile = tiles[Math.floor(Math.random() * tiles.length)];
+    return tile;
+  },
+  addColorClass(tile, color) {
+    tile.classList.add(color);
+  },
+  removeColorClass(tile, color) {
+    tile.classList.remove(color);
+  },
+}
 var gamePlay = {
-  yes: 0,
-  yesText: document.querySelector('.yesText'),
+  score: 0,
   checkForColor(tile) {
     if (tile.classList.length === 2) {
-      gamePlay.yes++;
+      gamePlay.score++;
       return tile;
     } else {
-      gamePlay.yes--;
-    }
-  },
-  yesTextColor(check) {
-    if (check) {
-      gamePlay.yesText.classList.remove('no');
-    } else {
-      if (!gamePlay.yesText.classList.contains('no')) {
-        gamePlay.yesText.classList.add('no');
+      if (gamePlay.score > 0) {
+        //gamePlay.score--;
       }
     }
   },
-  tallyScore() {
-    gamePlay.yesText.textContent = String(gamePlay.yes);
-  },
-  togglePulse(tile) {
-    tiles.forEach((tile) => {
-      tile.classList.remove('pulse');
+  updateScoreElements() {
+    var gameBoardScores = [].slice.call(document.querySelectorAll('.score'));
+    gameBoardScores.forEach(function (gameBoardScore) {
+      gameBoardScore.textContent = String(gamePlay.score);
     });
-    tile.classList.add('pulse');
+  },
+  highlightTile(tile, score) {
+    tile.classList.add('lit');
+    score.classList.add('animateScore');
+    setTimeout(function () {
+      tile.classList.remove('lit');
+      score.classList.remove('animateScore');
+    }, 500);
   },
 }
+// have to set the text of the score in all scores
+
+
 var tiles = [].slice.call(document.querySelectorAll('.tile'));
 // Click event for tiles
 tiles.forEach((tile) => {
   tile.addEventListener('click', function () {
-    //gamePlay.togglePulse(this);    
+    // Check to see if the clicked tile has color and increment/decrement gameplay.score
     var check = gamePlay.checkForColor(tile);
-    gamePlay.yesTextColor(check);
-    gamePlay.tallyScore();
+    // Update the html .score spans
+    gamePlay.updateScoreElements();
+    // Flash the points, set lite brite glow if check is true
+    if (check) {
+      gamePlay.highlightTile(tile, tile.firstElementChild);
+    }
   });
 });
 var audio = document.querySelector('.audio__player');
@@ -81,7 +86,7 @@ playButton.addEventListener('click', function () {
   try {
     audio.play();
     setTimeout(() => {
-      flashColor();
+      startFlash();
     }, 1100);
   } catch (ev) {
     console.log(ev);
@@ -92,10 +97,3 @@ var pause = document.querySelector('.pause');
 pause.addEventListener('click', function () {
   audio.pause()
 });
-/*
-60 bpm means 1 beat occurs per second
-console.log((60000 / 141) * 2);
-convert that to milliseconds 60,000*bpm
-Let the speed of the setTimeout be equal to either a difficulty setting, progression of game or BPM of song or something like that?
-based on bpm dynamic colors change each time
-*/

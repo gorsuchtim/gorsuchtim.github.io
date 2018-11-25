745bcc29ac510987709eaad1e559412378907d9e
Tim Gorsuch <gorsuchtim@gmail.com>
2018-11-25 12:14:33 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,55 @@
 'use strict'
+
+var elements = {
+  tiles: [].slice.call(document.querySelectorAll('.tile')),
+  titleScore: document.querySelector('.score--title'),
+  titleStreak: document.querySelector('.streak--title'),
+  playButton: document.querySelector('.play'),
+  gameBoardScores: [].slice.call(document.querySelectorAll('.score')),
+  gameBoardStreaks: [].slice.call(document.querySelectorAll('.streak')),
+  songChoice: document.querySelector('.songChoice'),
+  wrapTimer: document.querySelector('.wrap__timer'),
+}
+// need a variable to see if player hit all of the syncs so if that var === syncLength then they get like +50
+var dynamicVars = {
+  score: 0,
+  streak: 0,
+  syncLength: undefined,
+  startSync: undefined,
+  syncCount: 0,
+  beatCount: 0,
+}
+var utilities = {
+  clearClasses(elements) {
+    elements.forEach((element) => {
+      element.classList = element.classList[0];
+    });
+  },
+  createRandomNumber(max, min, v) {
+    if (v) {
+      return v = Math.floor(Math.random() * (max - min) + min);
+    } else {
+      return Math.floor(Math.random() * (max - min) + min);
+    }
+  },
+  swapClass(element, ...className) {
+    return element.classList.toggle(...className);
+  },
+  // failing here - running late after it adds the class
+  delaySwap(element, className) {
+    utilities.swapClass(element, className);
+    setTimeout(() => {
+      utilities.swapClass(element, className);
+    }, 500);
+  },
+  checkClass(element, className) {
+    if (element.classList.contains(className) === true) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+}
 var music = {
   audio: document.querySelector('.audio__player'),
   bpm: [],
@@ -17,82 +68,46 @@ var music = {
     }
   },
 }
-
-var elements = {
-  tiles: [].slice.call(document.querySelectorAll('.tile')),
-  titleScore: document.querySelector('.score--title'),
-  titleStreak: document.querySelector('.streak--title'),
-  playButton: document.querySelector('.play'),
-  gameBoardScores: [].slice.call(document.querySelectorAll('.score')),
-  gameBoardStreaks: [].slice.call(document.querySelectorAll('.streak')),
-  songChoice: document.querySelector('.songChoice'),
-  wrapTimer: document.querySelector('.wrap__timer'),
-}
-var dynamicVars = {
-  score: 0,
-  streak: 0,
-  syncLength: undefined,
-  startSync: undefined,
-  syncCount: 0,
-  beatCount: 0,
-}
-var utilities = {
-  clearClass(elements, className) {
-    elements.forEach((element) => {
-      element.classList.remove(className);
-    });
+var animations = {
+  beatFlash() {
+    utilities.clearClasses(elements.tiles);
+    gamePlay.checkSyncCount();
+    dynamicVars.beatCount++;
+    var randomTile = utilities.createRandomNumber(elements.tiles.length, 0);
+    utilities.swapClass(elements.tiles[randomTile], 'lit--blue', 'beatFlash');
+    gamePlay.beatOrSync();
   },
-  createRandomNumber(max, min, v) {
-    if (v) {
-      return v = Math.floor(Math.random() * (max - min) + min);
+  syncFlash() {
+    utilities.clearClasses(elements.tiles);
+    if (dynamicVars.syncCount < dynamicVars.syncLength) {
+      dynamicVars.syncCount++;
+      var randomTile = utilities.createRandomNumber(elements.tiles.length, 0);
+      utilities.swapClass(elements.tiles[randomTile], 'lit--purple', 'syncFlash');
+      gamePlay.beatOrSync();
     } else {
-      return Math.floor(Math.random() * (max - min) + min);
-    }
-  },
-  swapClass(element, className) {
-    return element.classList.toggle(className);
-  },
-  delaySwap(element, className) {
-    utilities.swapClass(element, className);
-    setTimeout(() => {
-      utilities.swapClass(element, className);
-    }, 500);
-  },
-  checkClass(element, className) {
-    if (element.classList.contains(className) === true) {
-      return true;
-    } else {
-      return false;
+      gamePlay.resetCounts();
+      animations.beatFlash();
     }
   },
 }
 var gamePlay = {
-  beatFlash() {
-    utilities.clearClass(elements.tiles, 'lit--blue');
-    utilities.clearClass(elements.tiles, 'lit--green');
-    gamePlay.checkSyncCount();
-    dynamicVars.beatCount++;
-    var randomTile = utilities.createRandomNumber(elements.tiles.length, 0);
-    utilities.swapClass(elements.tiles[randomTile], 'lit--blue');
-    gamePlay.beatOrSync();
-  },
   beatOrSync() {
     if (dynamicVars.beatCount < dynamicVars.startSync) {
       setTimeout(() => {
-        gamePlay.beatFlash();
+        animations.beatFlash();
       }, music.bpm[1]);
     } else {
       setTimeout(() => {
-        gamePlay.syncFlash();
+        animations.syncFlash();
       }, music.bpm[1] * 0.75);
     }
   },
   checkForColor(tile) {
-    var check = utilities.checkClass(tile, 'lit--blue');
+    var check = utilities.checkClass(tile, 'lit--blue') || utilities.checkClass(tile, 'lit--purple');
     if (check) {
-      utilities.delaySwap(tile, 'getBig');
+      // utilities.delaySwap(tile, 'getBig');
       utilities.delaySwap(tile.firstElementChild, 'animateScore');
-      gamePlay.increaseScore();
+      gamePlay.increaseScore(tile);
       gamePlay.streakBonus(tile);
     } else {
       dynamicVars.streak = 0;
@@ -106,8 +121,12 @@ var gamePlay = {
       dynamicVars.syncLength = utilities.createRandomNumber(12, 3, dynamicVars.syncLength);
     }
   },
-  increaseScore() {
-    dynamicVars.score++;
+  increaseScore(tile) {
+    if (tile.classList.contains('syncFlash')) {
+      dynamicVars.score = dynamicVars.score + 3;
+    } else {
+      dynamicVars.score++;
+    }
     dynamicVars.streak++;
   },
   resetCounts() {
@@ -115,7 +134,6 @@ var gamePlay = {
     dynamicVars.beatCount = 0;
   },
   startCountdown() {
-
     setTimeout(() => {
       utilities.swapClass(elements.wrapTimer, 'hidden');
       timer.timedCount();
@@ -124,7 +142,7 @@ var gamePlay = {
 
     setTimeout(() => {
       utilities.swapClass(elements.wrapTimer, 'hidden');
-      gamePlay.beatFlash();
+      animations.beatFlash();
       //music is coming in too late after first flash
     }, 3000 + music.bpm[0]); // + music.bpm[0]
   },
@@ -141,19 +159,6 @@ var gamePlay = {
     } else if (dynamicVars.streak === 100) {
       dynamicVars.score += 100;
       utilities.delaySwap(tile.firstElementChild.nextElementSibling, 'animateScore');
-    }
-  },
-  syncFlash() {
-    utilities.clearClass(elements.tiles, 'lit--blue');
-    utilities.clearClass(elements.tiles, 'lit--green');
-    if (dynamicVars.syncCount < dynamicVars.syncLength) {
-      dynamicVars.syncCount++;
-      var randomTile = utilities.createRandomNumber(elements.tiles.length, 0);
-      utilities.swapClass(elements.tiles[randomTile], 'lit--green');
-      gamePlay.beatOrSync();
-    } else {
-      gamePlay.resetCounts();
-      gamePlay.beatFlash();
     }
   },
   updateScoreElements() {
@@ -190,7 +195,6 @@ elements.songChoice.addEventListener('change', function () {
 music.setBPM('synth1'); // page load, load figaro by default
 
 /*
-need to add extra points (2x) for hitting the sync flashes
 add bonus 25+ points or 50+ if you hit all of them
 maybe that should be like 10 x syncLength so if SL is 8 then it would be 80 bonus points
 need a stop button 

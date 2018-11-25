53339483ae91565ba7a6be641f321c59abf90fe2
Tim Gorsuch <gorsuchtim@gmail.com>
2018-11-24 20:53:31 -0600

    asdf

--- js/script.js
+++ js/script.js
@@ -1,188 +1,190 @@
 'use strict'
-
-// Page elements
-var tiles = [].slice.call(document.querySelectorAll('.tile'));
-var titleScore = document.querySelector('.score--title');
-var titleStreak = document.querySelector('.streak--title');
-var gameBoardScores = [].slice.call(document.querySelectorAll('.score'));
-var gameBoardStreaks = [].slice.call(document.querySelectorAll('.streak'));
-
-// Dynamic variables
-var score = 0;
-var streak = 0;
-var syncLength = undefined;
-var startSync = undefined;
-var syncCount = 0;
-var beatCount = 0;
-
-// Global functions
-function countDownStart() {
-  classSwap(document.querySelector('.wrap__timer'), 'hidden');
-  timer.timedCount();
-  setTimeout(function () {
-    classSwap(document.querySelector('.wrap__timer'), 'hidden');
-    beatFlash();
-  }, 5000 + bpm[0]); // set as 5000 + bpm[0]?
-}
-function clearClass(elements, className) {
-  elements.forEach((element) => {
-    element.classList.remove(className);
-  });
-}
-function createRandomNumber(max, min) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-function classSwap(element, className) {
-  return element.classList.toggle(className);
-}
-function checkForClass(element, className) {
-  element.classList.contains(className) ? true : false;
-}
-function randomizeSyncVars(v, max, min) {
-  v = createRandomNumber(max, min);
-  return v;
-}
-function checkSyncCount() {
-  // if syncCount is reset to 0 then set goal and length
-  if (beatCount === 0) {
-    startSync = randomizeSyncVars(startSync, 20, 10);
-    syncLength = randomizeSyncVars(syncLength, 12, 3);
-  }
-}
-function beatOrSync() {
-  if (beatCount < startSync) {
-    setTimeout(function () {
-      beatFlash();
-    }, bpm[1]);
-  } else {
-    setTimeout(function () {
-      syncFlash();
-    }, bpm[1] * 0.5);
-  }
-}
-function resetCounts() {
-  syncCount = 0;
-  beatCount = 0;
-}
-function lightTile(color) {
-  var randomNumber = createRandomNumber(tiles.length, 0);
-  var litTile = classSwap(tiles[randomNumber], color);
-}
-
-// THE FIRST TIME SYNCFLASH RUNS HAS TO BE AT THE SAME TIMEOUT AS BEAT FLASH?
-function beatFlash() {
-  clearClass(tiles, 'neon--blue');
-  clearClass(tiles, 'lit--green');
-  checkSyncCount();
-  beatCount++;
-  lightTile('neon--blue');
-  console.log(beatCount, startSync, 'running beatflash');
-  beatOrSync();
-}
-function syncFlash() {
-  clearClass(tiles, 'neon--blue');
-  clearClass(tiles, 'lit--green');
-  if (syncCount < syncLength) {
-    syncCount++;
-    lightTile('lit--green');
-    console.log(syncCount, syncLength, 'running syncflash');
-    beatOrSync();
-  } else {
-    resetCounts();
-    beatFlash();
-  }
-}
-function checkStreakBonus(streak) {
-  if (streak === 10) {
-    score += 10;
-  } else if (streak === 20) {
-    score += 20;
-  } else if (streak === 50) {
-    score += 50;
-  } else if (streak === 100) {
-    score += 100;
-  }
-}
-function updateScoreElements() {
-  titleScore.textContent = String(`Points: ${score}`);
-  titleStreak.textContent = String(`Streak: ${streak}`);
-  var gameBoardScores = [].slice.call(document.querySelectorAll('.score'));
-  gameBoardScores.forEach(function (gameBoardScore) {
-    gameBoardScore.textContent = String(score);
-  });
-  var gameBoardStreaks = [].slice.call(document.querySelectorAll('.streak'));
-  gameBoardStreaks.forEach(function (gameBoardStreak) {
-    gameBoardStreak.textContent = String(streak);
-  });
-}
-function checkForColor(tile) {
-  if (tile.classList.length === 2) {
-    score++;
-    streak++;
-    checkStreakBonus(streak);
-    return tile;
-  } else {
-    streak = 0;
-  }
-}
-function showStreak(streak) {
-  streak.classList.add('animateScore');
-  setTimeout(function () {
-    streak.classList.remove('animateScore');
-  }, 500);
-}
-function highlightTile(tile, score) {
-  tile.classList.add('lit--blue');
-  score.classList.add('animateScore');
-  setTimeout(function () {
-    tile.classList.remove('lit--blue');
-    score.classList.remove('animateScore');
-  }, 500);
-}
-// Tile click events
-tiles.forEach((tile) => {
-  tile.addEventListener('click', function () {
-    // Check to see if the clicked tile has color and increment/decrement gameplay.score
-    var check = checkForColor(tile);
-    // Update the html .score spans
-    updateScoreElements();
-    // Flash the points, set lite brite glow if check is true
-    if (check) {
-      highlightTile(tile, tile.firstElementChild);
-      if (streak === 10 || streak === 20 || streak === 50) {
-        showStreak(tile.firstElementChild.nextElementSibling);
-      }
+var music = {
+  audio: document.querySelector('.audio__player'),
+  bpm: [],
+  setSong(choice) {
+    audio.pause();
+    audio.setAttribute('src', `audio/${choice}.mp3`);
+  },
+  setBPM(choice) {
+    // bpm[0] is delay to start actual song after countdown.  bpm[1] is actual bpm of song offset by transition timing
+    music.bpm = '';
+    if (choice === 'synth1') {
+      // synth1 is 120bpm
+      music.bpm = [1500, 880];
+    } else if (choice === 'bach') {
+      music.bpm = [1150, 825];
     }
+  },
+}
+// add neon shadow on highlighting tile and add scaling on tapping
+var elements = {
+  tiles: [].slice.call(document.querySelectorAll('.tile')),
+  titleScore: document.querySelector('.score--title'),
+  titleStreak: document.querySelector('.streak--title'),
+  playButton: document.querySelector('.play'),
+  gameBoardScores: [].slice.call(document.querySelectorAll('.score')),
+  gameBoardStreaks: [].slice.call(document.querySelectorAll('.streak')),
+  songChoice: document.querySelector('.songChoice'),
+  wrapTimer: document.querySelector('.wrap__timer'),
+}
+var dynamicVars = {
+  score: 0,
+  streak: 0,
+  syncLength: undefined,
+  startSync: undefined,
+  syncCount: 0,
+  beatCount: 0,
+}
+var utilities = {
+  clearClass(elements, className) {
+    elements.forEach((element) => {
+      element.classList.remove(className);
+    });
+  },
+  createRandomNumber(max, min, v) {
+    if (v) {
+      return v = Math.floor(Math.random() * (max - min) + min);
+    } else {
+      return Math.floor(Math.random() * (max - min) + min);
+    }
+  },
+  swapClass(element, className) {
+    return element.classList.toggle(className);
+  },
+  delaySwap(element, className) {
+    utilities.swapClass(element, className);
+    setTimeout(() => {
+      utilities.swapClass(element, className);
+    }, 500);
+  },
+  checkClass(element, className) {
+    if (element.classList.contains(className) === true) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+}
+var gamePlay = {
+  beatFlash() {
+    utilities.clearClass(elements.tiles, 'lit--blue');
+    utilities.clearClass(elements.tiles, 'lit--green');
+    gamePlay.checkSyncCount();
+    dynamicVars.beatCount++;
+    var randomTile = utilities.createRandomNumber(elements.tiles.length, 0);
+    utilities.swapClass(elements.tiles[randomTile], 'lit--blue');
+    gamePlay.beatOrSync();
+  },
+  beatOrSync() {
+    if (dynamicVars.beatCount < dynamicVars.startSync) {
+      setTimeout(() => {
+        gamePlay.beatFlash();
+      }, music.bpm[1]);
+    } else {
+      setTimeout(() => {
+        gamePlay.syncFlash();
+      }, music.bpm[1] * 0.75);
+    }
+  },
+  checkForColor(tile) {
+    var check = utilities.checkClass(tile, 'lit--blue');
+    if (check) {
+      utilities.delaySwap(tile, 'getBig');
+      utilities.delaySwap(tile.firstElementChild, 'animateScore');
+      gamePlay.increaseScore();
+      gamePlay.streakBonus(tile);
+    } else {
+      dynamicVars.streak = 0;
+      // use rightAnswer to add negative animation on the tap?
+    }
+  },
+  checkSyncCount() {
+    // if syncCount is reset to 0 then set goal and length
+    if (dynamicVars.beatCount === 0) {
+      dynamicVars.startSync = utilities.createRandomNumber(20, 10, dynamicVars.startSync);
+      dynamicVars.syncLength = utilities.createRandomNumber(12, 3, dynamicVars.syncLength);
+    }
+  },
+  increaseScore() {
+    dynamicVars.score++;
+    dynamicVars.streak++;
+  },
+  resetCounts() {
+    dynamicVars.syncCount = 0;
+    dynamicVars.beatCount = 0;
+  },
+  startCountdown() {
+
+    setTimeout(() => {
+      utilities.swapClass(elements.wrapTimer, 'hidden');
+      timer.timedCount();
+      music.audio.play();
+    }, 1100);
+
+    setTimeout(() => {
+      utilities.swapClass(elements.wrapTimer, 'hidden');
+      gamePlay.beatFlash();
+      //music is coming in too late after first flash
+    }, 3000 + music.bpm[0]); // + music.bpm[0]
+  },
+  streakBonus(tile) {
+    if (dynamicVars.streak === 10) {
+      dynamicVars.score += 10;
+      utilities.delaySwap(tile.firstElementChild.nextElementSibling, 'animateScore');
+    } else if (dynamicVars.streak === 20) {
+      dynamicVars.score += 20;
+      utilities.delaySwap(tile.firstElementChild.nextElementSibling, 'animateScore');
+    } else if (dynamicVars.streak === 50) {
+      dynamicVars.score += 50;
+      utilities.delaySwap(tile.firstElementChild.nextElementSibling, 'animateScore');
+    } else if (dynamicVars.streak === 100) {
+      dynamicVars.score += 100;
+      utilities.delaySwap(tile.firstElementChild.nextElementSibling, 'animateScore');
+    }
+  },
+  syncFlash() {
+    utilities.clearClass(elements.tiles, 'lit--blue');
+    utilities.clearClass(elements.tiles, 'lit--green');
+    if (dynamicVars.syncCount < dynamicVars.syncLength) {
+      dynamicVars.syncCount++;
+      var randomTile = utilities.createRandomNumber(elements.tiles.length, 0);
+      utilities.swapClass(elements.tiles[randomTile], 'lit--green');
+      gamePlay.beatOrSync();
+    } else {
+      gamePlay.resetCounts();
+      gamePlay.beatFlash();
+    }
+  },
+  updateScoreElements() {
+    elements.titleScore.textContent = String(`Points: ${dynamicVars.score}`);
+    elements.titleStreak.textContent = String(`Streak: ${dynamicVars.streak}`);
+
+    elements.gameBoardScores.forEach(function (gameBoardScore) {
+      gameBoardScore.textContent = String(dynamicVars.score);
+    });
+
+    elements.gameBoardStreaks.forEach(function (gameBoardStreak) {
+      gameBoardStreak.textContent = String(dynamicVars.streak);
+    });
+  },
+}
+
+// Tile click event
+elements.tiles.forEach((tile) => {
+  tile.addEventListener('click', function () {
+    gamePlay.checkForColor(tile);
+    gamePlay.updateScoreElements();
   });
 });
 // Play button event
-var playButton = document.querySelector('.play');
-playButton.addEventListener('click', function () {
-  countDownStart();
+elements.playButton.addEventListener('click', function () {
+  gamePlay.startCountdown();
 });
 // Audio events
-var songChoice = document.querySelector('.songChoice');
-songChoice.addEventListener('change', function () {
-  var choice = songChoice.value;
-  setSong(choice);
-  setBPM(choice);
+elements.songChoice.addEventListener('change', function () {
+  var choice = elements.songChoice.value;
+  music.setSong(choice);
+  music.setBPM(choice);
 });
-function setSong(choice) {
-  audio.pause();
-  if (choice === 'figaro') {
-    audio.setAttribute('src', 'audio/figaro.mp3');
-  } else if (choice === 'bach') {
-    audio.setAttribute('src', 'audio/bach.m4a');
-  }
-}
-var bpm = []; // 2 values.  try/catch settimeout & beatFlash settimeout
-setBPM('figaro'); // page load, load figaro by default
-function setBPM(choice) {
-  // bpm[0] is delay to start actual song after countdown.  bpm[1] is actual bpm of song offset by transition timing
-  bpm = '';
-  if (choice === 'figaro') {
-    bpm = [1100, 925];
-  } else if (choice === 'bach') {
-    bpm = [1150, 825];
-  }
-}
+music.setBPM('synth1'); // page load, load figaro by default
